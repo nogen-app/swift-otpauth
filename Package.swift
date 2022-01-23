660c946c3db278547d527a5d7a74c02454c6f8b8
Morten Nissen <morten@nogen.app>
2022-01-23 17:27:38 +0100

    renamed package

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "OTPAuth",
-            targets: ["OTPAuth"]),
+            name: "SwiftOTPAuth",
+            targets: ["SwiftOTPAuth"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,14 +23,14 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "OTPAuth",
+            name: "SwiftOTPAuth",
             dependencies: [
 							.product(name: "SwiftOTP", package: "SwiftOTP")
 						]),
         .testTarget(
-            name: "OTPAuthTests",
+            name: "SwiftOTPAuthTests",
             dependencies: [
-							"OTPAuth",
+							"SwiftOTPAuth",
 							.product(name: "SwiftOTP", package: "SwiftOTP")
 						]),
     ]
