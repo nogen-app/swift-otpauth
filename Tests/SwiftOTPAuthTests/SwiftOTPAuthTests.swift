e7dc1e50a357b3deedb492af561848174c6fc140
Morten Nissen <morten@nogen.app>
2022-01-25 16:29:00 +0100

    added missing test case

--- Tests/SwiftOTPAuthTests/SwiftOTPAuthTests.swift
+++ Tests/SwiftOTPAuthTests/SwiftOTPAuthTests.swift
@@ -14,7 +14,7 @@ final class SwiftOTPAuthTests: XCTestCase {
 	}
 	
 	func testTotpGenerate() throws {
-		let otp = OTPAuth(otpType: .totp, secret: "R33IQKVJ4TKX4PSDWRH7RESGCSPEORRU")
+		var otp = OTPAuth(otpType: .totp, secret: "R33IQKVJ4TKX4PSDWRH7RESGCSPEORRU")
 		
 		var date = DateComponents()
 		date.year = 1996
@@ -28,5 +28,18 @@ final class SwiftOTPAuthTests: XCTestCase {
 		let userCalendar = Calendar.current
 		
 		XCTAssertEqual(otp.generate(userCalendar.date(from: date)!), "660158")
+		
+		date.hour = 1
+		
+		XCTAssertNotEqual(otp.generate(userCalendar.date(from: date)!), "660158")
+	}
+	
+	func testHotpGenerate() throws {
+		var otp = OTPAuth(otpType: .hotp, secret: "R33IQKVJ4TKX4PSDWRH7RESGCSPEORRU", counter: 8 )
+		
+		XCTAssertEqual(otp.generate(), "991868")
+		//It should auto increment its counter so we should never get the same code twice in a row
+		XCTAssertNotEqual(otp.generate(), "991868")
+		
 	}
 }
